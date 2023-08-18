fa3e8a98ebe588d1e41971b91f97876325451815
Betty <bperezatencio@gmail.com>
2023-08-18 16:28:49 -0300

    Acceso público

---
+++ src/03-public.ts
@@ -0,0 +1,32 @@
+export class MyDate {
+  public year: number;
+  public month: number;
+  day: number;
+
+  constructor ( year: number, month: number, day: number) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
+
+  public printFormat(): string {                                         //Cree un metodo
+    return `${this.day}/${this.month}/${this.year}`;
+  };
+
+  public add(amount: number, type: 'days' | 'months' | 'years') {
+    if(type === 'days') {
+      this.day += amount;
+    }
+    if(type === 'months') {
+      this.month += amount;
+    }
+    if(type === 'years') {
+      this.year += amount;
+    }
+  };
+};
+
+const myDate = new MyDate(1993, 7, 9);                             //Acceso al metodo
+console.log(myDate.day);
+myDate.day = 12;
+console.log(myDate.day);
