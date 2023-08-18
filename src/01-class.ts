a39b67b3e16d48691931b36f7cd0fac4d94014d5
Betty <bperezatencio@gmail.com>
2023-08-18 11:48:21 -0300

    Class

---
+++ src/01-class.ts
@@ -0,0 +1,29 @@
+const date = new Date();
+date.getHours();
+date.getTime();
+date.toISOString();
+
+const date2 = new Date(1993, 1, 12);     // 0=enero, 11=diciembre
+date2.getHours();
+date2.getTime();
+date2.toISOString();
+
+console.log(date);
+console.log(date2);
+
+let MyVar;
+
+class MyDate {
+  year: number;
+  month: number;
+  day: number;
+
+  constructor ( year: number, month: number, day: number) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
+};
+
+const myDate = new MyDate(2023, 0, 17);   //Instancia de la class
+console.log(myDate);
