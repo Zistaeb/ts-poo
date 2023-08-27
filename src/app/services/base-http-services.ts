5a4d9cc410345e808989d41387b8033aa6f92dc9
Betty <bperezatencio@gmail.com>
2023-08-26 21:39:58 -0300

    Generics en métodos

--- src/app/services/base-http-services.ts
+++ src/app/services/base-http-services.ts
@@ -1,19 +1,28 @@
 import axios from 'axios';
 import { Category } from './../models/category-model';
 import { Product } from './../models/product-model';
+import { UpdateProductDto } from '../dtos/product-dto';
 
 export class BaseHttpService<TypeClass> {
   //data: TypeClass[] = [];
 
   constructor(
-    private url: string,
+    protected url: string,
   ) {}
 
-  async getAll(): Promise<TypeClass[]> {
+  async getAll() {
     const { data } = await axios.get<TypeClass[]>(this.url);
     return data;
   }
+
+  async update<ID, DTO>(id: ID, changes: DTO){
+    //const array: TypeClass[]=[];
+    const { data } = await axios.put(`${this.url}/${id}`, changes);
+    return data;
+  }
 }
+
+
 
 //const service = new BaseHttpService<string>();
 //service.data;
@@ -34,6 +43,9 @@ service2.getAll();*/
   const productService = new BaseHttpService<Product>(url1);
   const rta = await productService.getAll();
   console.log('products', rta.length);
+  productService.update<Product['id'], UpdateProductDto>(1, {
+    title: 'Super'
+  });
 
   const categoryService = new BaseHttpService<Category>(url2);
   const rta2 = await categoryService.getAll();
---
+++ src/app/services/product-http2-service.ts
@@ -0,0 +1,12 @@
+import { Product } from "../models/product-model";
+import { UpdateProductDto } from "../dtos/product-dto";
+import { BaseHttpService } from "./base-http-services";
+
+
+export class ProductHttpService extends BaseHttpService<Product> {
+  otherRequest() {
+    this.url;
+    //code
+    //code
+  }
+}
